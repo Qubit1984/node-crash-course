a75b64ed10b4156c3d442a6bc767bad916a25c1c
Qubit1984 <qubitphy@gmail.com>
2023-04-26 12:47:34 +0800

    提交信息

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views";
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      break;
+    case "/about":
+      path += "/about.html";
+      break;
+    default:
+      path += "/404.html";
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.statusCode = 200;
+      res.end(data);
+    }
+  });
 });
 
 // localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
